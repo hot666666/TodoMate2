d2dde0e275d6af05489ae3a4cd9ae3751d4842b6
hot666666 <hotcs6@naver.com>
2025-05-24 14:39:18 +0900

    feat : 앱 실행 시, Firebase 설정 - TodoMate2App

--- TodoMate2/TodoMate2App.swift
+++ TodoMate2/TodoMate2App.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct TodoMate2App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	init() {
+		guard ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] != "1" else { return }
+		FirebaseApp.configure()
+	}
+	
+	var body: some Scene {
+		WindowGroup {
+			// entry point of the app
+		}
+	}
+	
 }
